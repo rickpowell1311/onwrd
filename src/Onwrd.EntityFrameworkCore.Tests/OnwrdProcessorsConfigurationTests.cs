e242cd55f70e9b8f1e9b051ad1deedba21aa0b80
rickpowell1311 <rickpowell1311@gmail.com>
2023-01-25 15:24:40 +0000

    Configure and register onwrd processors

---
+++ src/Onwrd.EntityFrameworkCore.Tests/OnwrdProcessorsConfigurationTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.CompilerServices;
+using Xunit;
+
+namespace Onwrd.EntityFrameworkCore.Tests
+{
+    public class OnwrdProcessorsConfigurationTests
+    {
+        [Fact]
+        public void Register_WhenAlreadyRegistered_OnlyHasOneRegistrationInLibrary()
+        {
+            var onwrdProcessorsConfiguration = new OnwrdProcessorsConfiguration();
+            onwrdProcessorsConfiguration.Register<TestEvent, TestProcessor>();
+            onwrdProcessorsConfiguration.Register<TestEvent, TestProcessor>();
+
+            Assert.Single(onwrdProcessorsConfiguration
+                .Library[typeof(TestEvent)]);
+        }
+
+        [Fact]
+        public void Register_WhenScanningAssembly_IncludesTestEventAndTestProcessorsInLibrary()
+        {
+            var onwrdProcessorsConfiguration = new OnwrdProcessorsConfiguration();
+            onwrdProcessorsConfiguration.ScanAssemblies(typeof(TestProcessor).Assembly);
+
+            var result = onwrdProcessorsConfiguration.Library[typeof(TestEvent)];
+
+            Assert.Single(result);
+        }
+
+        public class TestEvent { }
+
+        public class TestProcessor : IOnwardProcessor<TestEvent>
+        {
+            public bool HasProcessed { get; private set; }
+
+            public Task Process(TestEvent @event, EventMetadata eventMetadata, CancellationToken cancellationToken = default)
+            {
+                HasProcessed = true;
+
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
---
+++ src/Onwrd.EntityFrameworkCore.Tests/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using static Onwrd.EntityFrameworkCore.Tests.EndToEndTests;
+
+namespace Onwrd.EntityFrameworkCore.Tests
+{
+    public class ServiceCollectionExtensionsTests
+    {
+        [Fact]
+        public void AddDbContext_WhenScanningForOnwardProcessors_DoesNotThrow()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddDbContext<TestContext>(
+                (_, builder) =>
+                {
+                    builder.UseInMemoryDatabase("ServiceCollectionExtensionsTests");
+                }, onwrd =>
+                {
+                    onwrd.UseOnwardProcessors(cfg =>
+                    {
+                        cfg.ScanAssemblies(typeof(ServiceCollectionExtensionsTests).Assembly);
+                    });
+                });
+        }
+
+        internal class TestContext : DbContext
+        {
+            public DbSet<TestEntity> TestEntities { get; set; }
+
+            public TestContext(DbContextOptions<TestContext> options) : base(options)
+            {
+            }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                var testEntityConfig = modelBuilder.Entity<TestEntity>();
+
+                testEntityConfig.ToTable("TestEntity", "ServiceCollectionExtensionsTests");
+
+                testEntityConfig.HasKey(x => x.Id);
+
+                testEntityConfig
+                    .Property(x => x.Id)
+                    .ValueGeneratedNever();
+
+                base.OnModelCreating(modelBuilder);
+            }
+        }
+
+        internal class TestEntity : EventRaiser
+        {
+            public Guid Id { get; set; }
+
+            public TestEntity()
+            {
+                Id = Guid.NewGuid();
+            }
+
+            public void RaiseEvent()
+            {
+                RaiseEvent(new TestEvent("TestEntity"));
+            }
+        }
+
+        internal class TestEvent
+        {
+            public string Greeting { get; set; }
+
+            public TestEvent()
+            {
+            }
+
+            public TestEvent(string sender)
+            {
+                Greeting = $"Hello from {sender}";
+            }
+        }
+
+        internal class TestOnwardProcessor : IOnwardProcessor
+        {
+            private readonly ProcessedEventAudit processedEventAudit;
+
+            public TestOnwardProcessor(ProcessedEventAudit processedEventAudit)
+            {
+                this.processedEventAudit = processedEventAudit;
+            }
+
+            public Task Process<T>(T @event, EventMetadata eventMetadata, CancellationToken cancellationToken = default)
+            {
+                this.processedEventAudit.Audit(@event);
+
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
